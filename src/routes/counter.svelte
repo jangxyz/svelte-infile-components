7e2f4cc661cb9785eb1f18c5396e8ee71b08e8d9
Kim, Jang-hwan (김장환) <janghwan@gmail.com>
2024-11-19 15:48:05 +0900

    add: simple counter example as a starting point.

---
+++ src/routes/counter.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { MouseEventHandler } from 'svelte/elements';
+
+	let { count = 0, onclick }: { count?: number; onclick: MouseEventHandler<HTMLButtonElement> } =
+		$props();
+</script>
+
+<button {onclick}>{count}</button>
