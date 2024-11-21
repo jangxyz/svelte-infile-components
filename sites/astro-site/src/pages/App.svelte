034b87bb748e66cd56333b2b2fa4116c40cfd956
Kim, Jang-hwan (김장환) <janghwan@gmail.com>
2024-11-21 16:04:41 +0900

    add. svelte component with infile components
    so it turns out that there is no room for separator collision,
    since the infile syntax lives only inside the svelte component,
    and not astro's.

---
+++ sites/astro-site/src/pages/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  //import Counter from "./Counter.svelte";
+  import Counter from "infile:MyCounter.svelte";
+</script>
+
+<Counter />
+
+---
+
+<template id="MyCounter">
+  <script lang="ts">
+    let count: number = $state(0);
+    const increment = () => {
+      count += 1;
+    };
+  </script>
+
+  <button onclick={increment}>
+    count is {count}
+  </button>
+</template>
