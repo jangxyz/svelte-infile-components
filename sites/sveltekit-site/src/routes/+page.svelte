5c98501b2f745a5482bc65c77d0a5bfd45f1d097
Kim, Jang-hwan (김장환) <janghwan@gmail.com>
2025-01-06 13:05:23 +0900

    app. wrap additinal svelte components

--- sites/sveltekit-site/src/routes/+page.svelte
+++ sites/sveltekit-site/src/routes/+page.svelte
@@ -1,30 +1,8 @@
 <script lang="ts">
-	//import Counter from './counter.svelte';
-	import Counter from 'infile:MyCounter.svelte';
-
-	let count = $state(0);
+    import App from './App.svelte';
 </script>
 
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<App />
 
-<Counter
-	{count}
-	onclick={() => {
-		count += 1;
-	}}
-/>
-
----
-
-<template id="MyCounter">
-	<script>
-		let count = $state(0);
-	</script>
-	<button onclick={() => (count += 1)}>Count here: {count}</button>
-</template>
-
----
-
-<template id="other"> Other. </template>
+<style>
+</style>
--- sites/sveltekit-site/src/routes/app.svelte
+++ sites/sveltekit-site/src/routes/app.svelte
@@ -1,23 +1,54 @@
 <script lang="ts">
-	import Counter from './counter.svelte';
-	//import Counter from 'infile:MyCounter.svelte';
+	//import Counter from './Counter.svelte';
+	import Counter from 'infile:MyCounter.svelte';
+	//import Other from 'infile:other.svelte';
 
 	let count = $state(0);
 </script>
 
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation!4</p>
+
+<button>Click me</button>
 
 <Counter
 	{count}
 	onclick={() => {
 		count += 1;
-	}}
-/>
+	}}>count: {count}</Counter
+>
+
+<style>
+	button {
+		padding: 0.5rem 1rem;
+		background-color: white;
+	}
+</style>
 
 ---
 
 <template id="MyCounter">
-	<button>Count here</button>
+	<script>
+	  import Other from 'infile:other.svelte';
+		let count = $state(0);
+	</script>
+	<button onclick={() => (count += 1)}>Infile component count: {count}</button>
+
+	<p>
+	  And this is another infile component:
+
+		<Other />
+	</p>
+
+
+	<style>
+		button {
+			background-color: red;
+		}
+	</style>
 </template>
+
+---
+
+<template id="other">Other.5</template>
