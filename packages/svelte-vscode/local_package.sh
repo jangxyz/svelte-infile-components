826418504e484e27bd6e72801698b29f160e1744
Kim, Jang-hwan (김장환) <janghwan@gmail.com>
2025-01-11 01:35:47 +0900

    add: local packagaing script

---
+++ packages/svelte-vscode/local_package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#npm run vscode:prepublish
+
+# package without dependency check
+vsce package --no-dependencies
+
+# manually shove node_modules into the package
+vsix_package=$(ls -1t svelte-infile-component-vscode-*.vsix | head -1)
+rm -rf extension && mkdir extension && mv node_modules extension/.
+zip -r "$vsix_package" extension/node_modules > /dev/null
+mv extension/node_modules . && rmdir extension
+
+echo "$vsix_package"
+
+# publish
+#vsce publish --packagePath "$vsix_package"
